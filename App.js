563307ead019f022ce8295806d65547be73dfd4b
kunie <kunie.dev@gmail.com>
2022-07-17 20:13:24 +0900

    #2.2 AppLoading part Two

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
+import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
-import { Text } from 'react-native';
+import { Text, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Asset } from 'expo-asset';
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await new Promise((resolve) => {
-      setTimeout(resolve, 10000);
-    });
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync(require('./IMG_0025.jpeg'));
+    await Image.prefetch(
+      'https://cf.channel.io/thumb/200x200/pub-file/9053/608a69eb54f684f4f79e/profile_white_big.png'
+    );
   };
 
   if (!ready) {
